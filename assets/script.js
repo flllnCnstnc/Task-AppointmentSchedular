dc6608499178226d005b85ca0b4bc6eeed1729c2
coflu <cofluellen@gmail.com>
2022-10-23 10:56:00 -0500

    Added a js file link to html and corrected the css file link

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
 // Todays date
-var currentDay = moment();
-$("#1a").text(today.format('dddd'));
+var today = moment();
+$("#1a").text(today.format("MMM Do, YYYY"));
+
+var dayWeek = today.format("[Today is] dddd")
+$("#2a").text(dayWeek);
